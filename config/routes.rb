9c457bf27a51968cbf5058a3b1872a9d4c2920c0
juju8899 <146299878+juju8899@users.noreply.github.com>
2023-10-19 12:50:12 +0000

    Bookers-view一部作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   get 'homes/about' => 'homes#about', as: 'about'
   devise_for :users
   resources :books, only: [:index, :create, :show]
-  resources :users, only: [:show, :edit, :index]
+  resources :users, only: [:show, :edit, :update, :index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
