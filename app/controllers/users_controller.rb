3dd8affec21ce77cebcc0a08a0cb200b76a1493d
juju8899 <146299878+juju8899@users.noreply.github.com>
2023-10-21 06:34:13 +0000

    Bookersレイアウト

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,16 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @books = @user.books
+    @book = Book.new
   end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(@book.id)
+  end
+
 
   def edit
     @user = User.find(params[:id])
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   get 'homes/about' => 'homes#about', as: 'about'
   devise_for :users
   resources :books, only: [:index, :create, :show, :destroy]
-  resources :users, only: [:show, :edit, :update, :index]
+  resources :users, only: [:show, :create, :edit, :update, :index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
