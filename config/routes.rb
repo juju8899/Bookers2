40c7d237c7aa1d464416dc2cfc30a8e1c85b0f6f
juju8899 <146299878+juju8899@users.noreply.github.com>
2023-11-08 16:13:35 +0000

    [Finish]Bookers2/いいね・コメント機能

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :books, only: [:index, :show, :create, :edit, :update, :destroy] do
     resource :favorite, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
   end
   resources :users, only: [:index, :show, :create, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
