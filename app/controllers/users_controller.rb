f84df377388e1117bda4f1cdfed1a9325d645c93
juju8899 <146299878+juju8899@users.noreply.github.com>
2023-11-10 14:31:14 +0000

    Bookers2/フォロー機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,6 +32,16 @@ class UsersController < ApplicationController
     else
       render :edit
     end
+    
+    def followings
+      user = User.find(params[:id])
+      @users = user.followings
+    end
+    
+    def followers
+      user = User.find(params[:id])
+      @users = user.followers
+    end
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
     resource :favorite, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
   end
-  resources :users, only: [:index, :show, :create, :edit, :update]
+  resources :users, only: [:index, :show, :create, :edit, :update] do
+    resource :relationships, only: [:create, :destroy]
+    get :followings, on: :member
+    get :followers, on: :member
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
